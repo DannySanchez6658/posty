dff455b28798b4b667046513d0ec15897fbed24c
danny <danny@DannyX.myguest.virtualbox.org>
2023-11-28 00:25:13 +0300

    [Feature]: Add the user and post model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    post = models.TextField()
