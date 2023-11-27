75983712a484cb57accad84356e1978e733f6ffd
danny <danny@DannyX.myguest.virtualbox.org>
2023-11-28 00:29:52 +0300

    [Update]: Add the foreign key property to post

--- base/models.py
+++ base/models.py
@@ -5,5 +5,5 @@ class User(models.Model):
     user_name = models.CharField(max_length=20)
 
 class Post(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     post = models.TextField()
